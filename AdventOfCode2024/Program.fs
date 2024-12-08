d92e218262865283dc53ad521a716531308b658d
stefan jonsson <stepson_jonsson@hotmail.com>
2024-12-08 22:53:08 +0100

    feat: implement day one solutions

--- AdventOfCode2024/Program.fs
+++ AdventOfCode2024/Program.fs
@@ -1,2 +1,6 @@
 ﻿module AdventOfCode2024.Program
 
+[<EntryPoint>]
+day1partOne.main |> ignore
+day1partTwo.main |> ignore
+0 |> ignore
--- AdventOfCode2024/day1partTwo.fs
+++ AdventOfCode2024/day1partTwo.fs
@@ -1,2 +1,15 @@
 ﻿module AdventOfCode2024.day1partTwo
 
+open day1Input
+
+let main =
+    input2
+    |> List.groupBy id
+    |> List.map (fun (k, v) -> k, List.length v)
+    |> Map.ofList
+    |> fun similarityMap ->
+        input
+        |> List.fold (fun acc x -> acc + (x * (Map.tryFind x
+            similarityMap |> Option.defaultValue 0))) 0
+        |> printfn "%d"
+    0
